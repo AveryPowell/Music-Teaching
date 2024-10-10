84df4075e87921cd3732f7f910e66f52fa02170c
AveryPowell <averyldpowell@gmail.com>
2024-10-09 22:44:46 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Log a message to the console
+console.log("Hello World from JavaScript!");
+
+// Example: Change text content after 2 seconds
+setTimeout(() => {
+    document.querySelector('h1').textContent = "Hello from JavaScript!";
+}, 2000);
